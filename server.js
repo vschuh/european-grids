7b68dac02e15192d0e952bec06bb79cc9b98ef4a
Schuh Valentin <56673098+vschuh@users.noreply.github.com>
2025-09-24 21:11:15 +0200

    ssl

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const pool = new Pool({
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 const serverSessionId = Date.now().toString()
