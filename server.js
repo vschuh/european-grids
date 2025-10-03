5c1fbb5b3b9847957a3204a75f6db6ef70c608d4
Schuh Valentin <56673098+vschuh@users.noreply.github.com>
2025-10-03 17:10:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -148,11 +148,11 @@ const buildCondition = (category, playerAlias = 'p', startingIndex = 1) => {
                 values = [category.value];
                 break;
             case 'perfect_game':
-                text = `EXISTS (SELECT 1 FROM player_record pr_pos JOIN player_game pg ON pg.playerid = pr_pos.id WHERE pg.pitch_cg = 1 and pg.pitch_h = 0 and pg.pitch_bb = 0 and pg.pitch_hbp = 0 and pg.pitch_ip >= $${startingIndex})`;
+                text = `EXISTS (SELECT 1 FROM player_record pr_pos JOIN player_game pg ON pg.playerid = pr_pos.id WHERE pr.playerid = ${alias} and pg.pitch_cg = 1 and pg.pitch_h = 0 and pg.pitch_bb = 0 and pg.pitch_hbp = 0 and pg.pitch_ip >= $${startingIndex})`;
                 values = [parseInt(category.value)];
                 break;
             case 'no_hitter':
-                text = `EXISTS (SELECT 1 FROM player_record pr_pos JOIN player_game pg ON pg.playerid = pr_pos.id WHERE pg.pitch_cg = 1 and pg.pitch_h = 0 and pg.pitch_ip >= $${startingIndex})`;
+                text = `EXISTS (SELECT 1 FROM player_record pr_pos JOIN player_game pg ON pg.playerid = pr_pos.id WHERE pr.playerid = ${alias} and pg.pitch_cg = 1 and pg.pitch_h = 0 and pg.pitch_ip >= $${startingIndex})`;
                 values = [parseInt(category.value)];
                 break;
             default:
